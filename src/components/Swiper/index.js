c2144f367d2e672b01b727adf3bc2295d5bef1bf
mhevyk <maxxevyk7@gmail.com>
2023-02-03 00:40:28 +0200

    Added Swiper component

---
+++ src/components/Swiper/index.js
@@ -0,0 +1,45 @@
+import './Swiper.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import { Navigation, Pagination } from 'swiper';
+import { Swiper as SwiperWrapper, SwiperSlide } from 'swiper/react';
+import SwiperButton from '../SwiperButton';
+
+function Swiper({ slides, isPagination = false, SlideItem }) {
+  const modules = [Navigation, Pagination];
+
+  const slideChangeHandler = () => {
+    console.log('slide change');
+  };
+
+  return (
+    <div className="swiper-container">
+      <SwiperButton variant="prev" />
+      <SwiperButton variant="next" />
+      <SwiperWrapper
+        modules={modules}
+        spaceBetween={50}
+        onSlideChange={slideChangeHandler}
+        navigation={{
+          nextEl: '.swiper-button__next',
+          prevEl: '.swiper-button__prev',
+          disabledClass: 'swiper-button__disabled',
+        }}
+        pagination={{
+          enabled: isPagination,
+          clickable: true,
+        }}
+      >
+        {slides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <SlideItem data={slide} />
+          </SwiperSlide>
+        ))}
+      </SwiperWrapper>
+    </div>
+  );
+}
+
+export default Swiper;
