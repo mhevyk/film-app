9740182c516caf10869923368b7ea2258a0b6b88
mhevyk <maxxevyk7@gmail.com>
2023-02-02 22:14:50 +0200

    Added main navigation bar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import './Navbar.css';
+import profileIcon from '../../assets/profile.png';
+import settingsIcon from '../../assets/settings.png';
+
+function Navbar() {
+  return (
+    <nav className="nav main__nav">
+      <a href="#profile">
+        <img src={profileIcon} alt="Profile" />
+      </a>
+      <a href="#settings">
+        <img src={settingsIcon} alt="Settings" />
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
