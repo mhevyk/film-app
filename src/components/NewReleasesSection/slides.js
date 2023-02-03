d89988b34a7123ca0f2a10cd2443511734362af0
mhevyk <maxxevyk7@gmail.com>
2023-02-03 20:14:24 +0200

    Added slides for NewReleases swiper

---
+++ src/components/NewReleasesSection/slides.js
@@ -0,0 +1,52 @@
+export const slides = [
+  {
+    genre: 'Fantasy',
+    image: 'assets/releases/wonder-woman-1984.jpg',
+    name: 'Wonder Woman 1984',
+  },
+  {
+    genre: 'Fantasy',
+    image: 'assets/releases/zack-snyders-justice-league.png',
+    name: 'Zack Snyder`s Justice League',
+  },
+  {
+    genre: 'Fantasy',
+    image: 'assets/releases/mortal-kombat.png',
+    name: 'Mortal Kombat',
+  },
+  {
+    genre: 'Drama',
+    image: 'assets/releases/the-courier.jfif',
+    name: 'The Courier'
+  },
+  {
+    genre: 'Action',
+    image: 'assets/releases/thunder-force.jpg',
+    name: 'Thunder Force'
+  },
+  {
+    genre: 'Family',
+    image: 'assets/releases/peter-rabbit-2-the-runaway.jpg',
+    name: 'Peter Rabbit 2: The Runaway'
+  },
+  {
+    genre: 'Action',
+    image: 'assets/releases/the-marksman.jpg',
+    name: 'The Marksman'
+  },
+  {
+    genre: 'Comedy',
+    image: 'assets/releases/tom-and-jerry.jpg',
+    name: 'Tom & Jerry'
+  },
+  {
+    genre: 'Horror',
+    image: 'assets/releases/the-unholy.png',
+    name: 'The Unholy'
+  },
+  {
+    genre: 'Horror',
+    image: 'assets/releases/the-conjuring.png',
+    name: 'The Conjuring: The Devil Made Me Do It'
+  }
+];
