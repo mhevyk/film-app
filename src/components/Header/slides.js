398f5a6a0424df878def241c633482b6fccb90e1
mhevyk <maxxevyk7@gmail.com>
2023-02-04 11:34:02 +0200

    Added new slides and id to each slide

--- src/components/Header/slides.js
+++ src/components/Header/slides.js
@@ -1,24 +1,27 @@
 export const slides = [
   {
+    id: 1,
     genre: 'Science Fiction',
     image: 'assets/header/godzilla-vs-kong.png',
     name: 'Godzilla vs. Kong',
     description: `In a time monsters walk the Earth, humanity's fight for its future sets Godzilla and Kong on a collision
-    course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.`,
+      course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.`,
   },
   {
+    id: 2,
     genre: 'Fantasy',
     image: 'assets/header/wonder-woman.png',
     name: 'Wonder Woman 1984',
     description: `A botched store robbery places Wonder Woman in a global battle against a powerful
-    and mysterious ancient force that puts her powers in jeopardy.`,
+      and mysterious ancient force that puts her powers in jeopardy.`,
   },
   {
+    id: 3,
     genre: 'Fantasy',
     image: 'assets/header/jack-snyders-justice-league.webp',
     name: 'Jack Snyder`s Justice League',
     description: `Determined to ensure Superman's ultimate sacrifice was not in vain,
-    Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans
-    to protect the world from an approaching threat of catastrophic proportions.`,
+      Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans
+      to protect the world from an approaching threat of catastrophic proportions.`,
   },
 ];
--- src/components/NewReleaseSection/slides.js
+++ src/components/NewReleaseSection/slides.js
@@ -1,50 +1,60 @@
 export const slides = [
   {
+    id: 1,
     genre: 'Fantasy',
     image: 'assets/releases/wonder-woman-1984.jpg',
     name: 'Wonder Woman 1984',
   },
   {
+    id: 2,
     genre: 'Fantasy',
     image: 'assets/releases/zack-snyders-justice-league.png',
     name: 'Zack Snyder`s Justice League',
   },
   {
+    id: 3,
     genre: 'Fantasy',
     image: 'assets/releases/mortal-kombat.png',
     name: 'Mortal Kombat',
   },
   {
+    id: 4,
     genre: 'Drama',
     image: 'assets/releases/the-courier.jfif',
     name: 'The Courier'
   },
   {
+    id: 5,
     genre: 'Action',
     image: 'assets/releases/thunder-force.jpg',
     name: 'Thunder Force'
   },
   {
+    id: 6,
     genre: 'Family',
     image: 'assets/releases/peter-rabbit-2-the-runaway.jpg',
     name: 'Peter Rabbit 2: The Runaway'
   },
   {
+    id: 7,
     genre: 'Action',
     image: 'assets/releases/the-marksman.jpg',
     name: 'The Marksman'
   },
   {
+    id: 8,
     genre: 'Comedy',
     image: 'assets/releases/tom-and-jerry.jpg',
     name: 'Tom & Jerry'
   },
   {
+    id: 9,
     genre: 'Horror',
     image: 'assets/releases/the-unholy.png',
     name: 'The Unholy'
   },
   {
+    id: 10,
     genre: 'Horror',
     image: 'assets/releases/the-conjuring.png',
     name: 'The Conjuring: The Devil Made Me Do It'
