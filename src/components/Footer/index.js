4f0940e75f5b43d4021c6026466cd490f330144f
mhevyk <maxxevyk7@gmail.com>
2023-02-04 11:41:38 +0200

    Added footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        Recreated{' '}
+        <a
+          href="https://www.figma.com/community/file/970595453636409922"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Figma template
+        </a>{' '}
+        by Maksym Hevyk, 2023
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
